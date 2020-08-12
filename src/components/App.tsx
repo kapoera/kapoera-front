1a44df36b181ced71309c658c2d3bdcb71c19962
pacokwon <haechank@gmail.com>
2020-08-13 02:12:41 +0900

    add: minimal ts / react setup

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App: React.FC = () => (
+  <div>
+    Kapoera App
+  </div>
+)
+
+export default App
---
+++ webpack.dev.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader",
+      },
+      {
+        exclude: /node_modules/,
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    port: 8000,
+    host: "0.0.0.0",
+    hot: true,
+    historyApiFallback: true,
+  },
+};
