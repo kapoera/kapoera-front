c2f2db953bd1cd55c95c5cea35415b6a5c9f66e8
pacokwon <haechank@gmail.com>
2020-08-15 17:18:45 +0900

    add: login page for fake login

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+const LoginForm: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = async () => {
+    console.log({ username, password });
+
+    try {
+      const result = axios({
+        method: 'POST',
+        url: 'http://aria.sparcs.org:32960/login',
+        data: { username, password }
+      });
+      console.log(result);
+    } catch (err) {}
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '300px'
+      }}
+    >
+      <h1>Login</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        Username
+        <input
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        Password
+        <input
+          type="text"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <button style={{ marginTop: '10px' }} onClick={submitHandler}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+const RegisterForm: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = () => {
+    try {
+      const result = axios({
+        method: 'POST',
+        url: 'http://aria.sparcs.org:32960/signup',
+        data: { username, nickname, password }
+      });
+      console.log(result);
+    } catch (err) {}
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '300px',
+        marginRight: '30px'
+      }}
+    >
+      <h1>Register</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        Username
+        <input
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        Nickname
+        <input
+          type="text"
+          value={nickname}
+          onChange={e => setNickname(e.target.value)}
+        />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        Password
+        <input
+          type="text"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <button style={{ marginTop: '10px' }} onClick={submitHandler}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+interface LoginPageProps {
+  className: string;
+}
+
+const LoginPage: React.FC<LoginPageProps> = ({ className }: LoginPageProps) => {
+  return (
+    <div
+      className={className}
+      style={{ display: 'flex', justifyContent: 'center' }}
+    >
+      <RegisterForm />
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
