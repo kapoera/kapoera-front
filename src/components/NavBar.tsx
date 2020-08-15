255672b3d0ea1efcf0e5a40a2deea53c9898c3ff
pacokwon <haechank@gmail.com>
2020-08-15 17:18:29 +0900

    add: padding to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,16 @@ import { useHistory } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 import styled from 'styled-components';
 
-const NavBar: React.FC = () => {
+interface NavBarProps {
+  className: string;
+}
+
+const NavBar: React.FC<NavBarProps> = ({ className }: NavBarProps) => {
   const history = useHistory();
   const [activeItem, setActiveItem] = useState('home');
 
   return (
-    <Menu color="blue" inverted secondary>
+    <Menu className={className} color="blue" inverted secondary>
       <Menu.Item
         name="home"
         active={activeItem === 'home'}
@@ -28,4 +32,6 @@ const NavBar: React.FC = () => {
   );
 };
 
-export default styled(NavBar)``;
+export default styled(NavBar)`
+  padding: 5px 20px !important;
+`;
