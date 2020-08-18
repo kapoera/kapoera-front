a28712e41358d4d9850385c262df05aa65dfcfbe
pacokwon <haechank@gmail.com>
2020-08-18 17:11:27 +0900

    update: display nickname on profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '@/context';
 
-const Profile: React.FC = () => <div>Profile page</div>;
+const Profile: React.FC = () => {
+  const { state, dispatch } = useContext(GlobalContext);
+  const {
+    user: { username, nickname }
+  } = state;
+
+  return <div>Profile page for {username}</div>;
+};
 
 export default Profile;
