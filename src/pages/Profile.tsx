fd9fa46687e4f094289ed9e1cabc65f225e7bc99
pacokwon <haechank@gmail.com>
2020-08-20 00:00:35 +0900

    update: change nickname if not taken

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,35 @@
-import React, { useContext, useState } from 'react';
-import { GlobalContext } from '@/context';
-import { Container, Input } from 'semantic-ui-react';
+import React, { useContext, useEffect, useState } from 'react';
+import { GlobalContext, Actions } from '@/context';
+import { Container, Input, Popup } from 'semantic-ui-react';
+import axios from '@/utils/axios';
 
 const Profile: React.FC = () => {
-  const { state } = useContext(GlobalContext);
+  const { state, dispatch } = useContext(GlobalContext);
   const { nickname, username } = state.user || { nickname: '', username: '' };
   const [inputNickname, setInputNickname] = useState<string>(nickname);
+  const [popupOpen, setPopupOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    setInputNickname(nickname);
+  }, [nickname]);
+
+  const handleNicknameSubmit = async () => {
+    if (nickname === '') return;
+
+    const result: {
+      success: boolean;
+      message?: string;
+    } = await axios.post('/api/nickname', { nickname });
+
+    if (result.success) {
+      dispatch({ type: Actions.UpdateNickname, payload: nickname });
+    } else {
+      setPopupOpen(true);
+      setTimeout(() => {
+        setPopupOpen(false);
+      }, 3000);
+    }
+  };
 
   return (
     <Container
@@ -13,14 +37,26 @@ const Profile: React.FC = () => {
     >
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <Input label="username" type="text" value={username} />
-        <Input
-          label="nickname"
-          type="text"
-          value={inputNickname || nickname}
-          onChange={(_, data) => {
-            setInputNickname(data.value);
-          }}
-          action={{ color: 'teal', content: 'Save Changes' }}
+        <Popup
+          trigger={
+            <Input
+              label="nickname"
+              type="text"
+              value={inputNickname}
+              onChange={(_, data) => {
+                setInputNickname(data.value);
+              }}
+              action={{
+                color: 'teal',
+                content: 'Save Changes',
+                onClick: handleNicknameSubmit
+              }}
+            />
+          }
+          content="Nickname taken. Please select another nickname"
+          position="top right"
+          open={popupOpen}
+          inverted
         />
       </div>
     </Container>
