da0813519a5a11c46a4d681c7ce1431f21322c76
pacokwon <haechank@gmail.com>
2020-08-21 17:07:25 +0900

    update: use grids + apply translations

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,20 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useIntl } from 'react-intl';
+import styled from 'styled-components';
+import { Container, Grid, Header, Input, Popup } from 'semantic-ui-react';
 import { GlobalContext, Actions } from '@/context';
-import { Container, Input, Popup } from 'semantic-ui-react';
 import axios from '@/utils/axios';
+
+const StyledHeader = styled(Header)`
+  margin-bottom: 30px !important;
+`;
 
 const Profile: React.FC = () => {
   const { state, dispatch } = useContext(GlobalContext);
   const { nickname, username } = state.user || { nickname: '', username: '' };
   const [inputNickname, setInputNickname] = useState<string>(nickname);
   const [popupOpen, setPopupOpen] = useState<boolean>(false);
+  const { formatMessage: f } = useIntl();
 
   useEffect(() => {
     setInputNickname(nickname);
@@ -36,30 +43,45 @@ const Profile: React.FC = () => {
     <Container
       style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
     >
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <Input label="username" type="text" value={username} />
-        <Popup
-          trigger={
-            <Input
-              label="nickname"
-              type="text"
-              value={inputNickname}
-              onChange={(_, data) => {
-                setInputNickname(data.value);
-              }}
-              action={{
-                color: 'teal',
-                content: 'Save Changes',
-                onClick: handleNicknameSubmit
-              }}
+      <StyledHeader as="h1">{f({ id: 'profile_header' })}</StyledHeader>
+      <Grid centered>
+        <Grid.Row centered>
+          <Grid.Column width={2} stretched verticalAlign="middle">
+            {f({ id: 'profile_username' })}
+          </Grid.Column>
+          <Grid.Column width={10} stretched verticalAlign="middle">
+            <Input type="text" value={username} />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered>
+          <Grid.Column width={2} stretched verticalAlign="middle">
+            {f({ id: 'profile_nickname' })}
+          </Grid.Column>
+          <Grid.Column width={10} stretched verticalAlign="middle">
+            <Popup
+              trigger={
+                <Input
+                  type="text"
+                  value={inputNickname}
+                  onChange={(_, data) => {
+                    setInputNickname(data.value);
+                  }}
+                  action={{
+                    color: 'teal',
+                    content: f({ id: 'profile_save_changes' }),
+                    onClick: handleNicknameSubmit,
+                    disabled: nickname === inputNickname
+                  }}
+                />
+              }
+              content="Nickname taken. Please try another nickname"
+              position="top right"
+              open={popupOpen}
+              inverted
             />
-          }
-          content="Nickname taken. Please try another nickname"
-          position="top right"
-          open={popupOpen}
-          inverted
-        />
-      </div>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Container>
   );
 };
