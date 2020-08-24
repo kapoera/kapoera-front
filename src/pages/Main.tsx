3e2d545cca959aea0c6f522b782749a62b23a67d
pacokwon <haechank@gmail.com>
2020-08-25 00:45:11 +0900

    add: transition to status overlay component

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Container, Grid, Header, Transition } from 'semantic-ui-react';
-import { GlobalContext } from '@/context';
+import React, { useEffect, useState } from 'react';
+import { Container, Grid, Transition } from 'semantic-ui-react';
 import axios from '@/utils/axios';
 import { GameCard, GameCardProps } from '@/components/GameCard';
 import StatusBanner from '@/components/StatusBanner';
 
 const Main: React.FC = () => {
-  const { state, dispatch } = useContext(GlobalContext);
   const [gamesData, setGamesData] = useState([]);
 
   useEffect(() => {
@@ -20,11 +18,12 @@ const Main: React.FC = () => {
 
   return (
     <Container>
-      <Header as="h1">Main</Header>
-      <StatusBanner />
+      <Transition transitionOnMount duration={300}>
+        <StatusBanner />
+      </Transition>
       <Grid columns={3} doubling stackable>
         {gamesData.map(data => (
-          <Transition key={data.game_type} transitionOnMount>
+          <Transition key={data.game_type} transitionOnMount duration={500}>
             <Grid.Column>
               <GameCard {...data} />
             </Grid.Column>
