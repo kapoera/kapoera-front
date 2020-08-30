e08657c6ee667b9d22800181e81dfcbb2fdea9db
pacokwon <haechank@gmail.com>
2020-08-30 20:23:48 +0900

    update: remove axios interceptors

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,36 +3,8 @@ import * as AuthUtils from '@/utils/auth';
 import config from '@/config';
 
 const instance = axios.create({
-  baseURL: config.baseURL
+  baseURL: config.baseURL,
+  withCredentials: true
 });
-
-instance.interceptors.request.use(config => {
-  const token = AuthUtils.getAccessToken();
-
-  config.headers.Authorization = token ? `Bearer ${token}` : '';
-
-  return config;
-});
-
-instance.interceptors.response.use(
-  response => response,
-  async error => {
-    const originalRequest = error.config;
-
-    if (
-      error.response.status === 403 &&
-      error.response.data.expired &&
-      !originalRequest._retry &&
-      AuthUtils.getRefreshToken() !== null
-    ) {
-      originalRequest._retry = true;
-      const accessToken = await AuthUtils.requestAccessToken();
-      AuthUtils.setAccessToken(accessToken);
-      return instance(originalRequest);
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default instance;
