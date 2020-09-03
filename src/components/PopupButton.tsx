1e13d262c7b21c3208dfa83da71c8f2bd8322dd0
pacokwon <haechank@gmail.com>
2020-09-03 23:27:31 +0900

    update: remove styles from Popup component

--- src/components/PopupButton.tsx
+++ src/components/PopupButton.tsx
@@ -3,11 +3,16 @@ import { Button } from 'semantic-ui-react';
 import { useIntl } from 'react-intl';
 
 const PopupButton = ({ ...rest }) => {
-    const { formatMessage: f } = useIntl();
-    return (
-        <Button circular color="grey" style={{ margin: "0 auto", width: "25%" }} content={f({ id: 'betting.button' })} {...rest}>
-        </Button>
-    )
-}
+  const { formatMessage: f } = useIntl();
+  return (
+    <Button
+      circular
+      color="grey"
+      content={f({ id: 'betting.button' })}
+      {...rest}
+    ></Button>
+  );
+};
 
 export default PopupButton;
+
