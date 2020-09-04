3b0c3ae252f34a89ff0acb32af27660775d7d070
pacokwon <haechank@gmail.com>
2020-09-04 14:52:48 +0900

    add: fetch and display rankinbgs on main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { Container, Grid, Transition } from 'semantic-ui-react';
 import axios from '@/utils/axios';
 import { GameCard, GameCardProps } from '@/components/GameCard';
+import { Billboard, RankingI } from '@/components/Billboard';
 import StatusBanner from '@/components/StatusBanner';
+
+interface RankingResponse {
+  success: boolean;
+  rankings?: RankingI[];
+}
 
 const Main: React.FC = () => {
   const [gamesData, setGamesData] = useState([]);
+  const [rankings, setRankings] = useState<RankingI[]>([]);
 
   useEffect(() => {
     const fetchGames = async () => {
@@ -13,7 +20,15 @@ const Main: React.FC = () => {
       setGamesData(data);
     };
 
+    const fetchRanking = async () => {
+      const { data }: { data: RankingResponse } = await axios.get(
+        '/api/rankings/top?limit=5'
+      );
+      setRankings(data.rankings);
+    };
+
     fetchGames();
+    fetchRanking();
   }, []);
 
   return (
@@ -21,15 +36,23 @@ const Main: React.FC = () => {
       <Transition transitionOnMount duration={300}>
         <StatusBanner />
       </Transition>
-      <Grid columns={3} doubling stackable>
-        {gamesData.map(data => (
-          <Transition key={data.game_type} transitionOnMount duration={500}>
-            <Grid.Column>
-              <GameCard {...data} />
-            </Grid.Column>
-          </Transition>
-        ))}
-      </Grid>
+      {rankings && <Billboard rankings={rankings} />}
+      <div>
+        <div
+          style={{ marginLeft: '30px', marginBottom: '40px', fontSize: '4rem' }}
+        >
+          Games
+        </div>
+        <Grid columns={3} doubling stackable>
+          {gamesData.map(data => (
+            <Transition key={data.game_type} transitionOnMount duration={500}>
+              <Grid.Column>
+                <GameCard {...data} />
+              </Grid.Column>
+            </Transition>
+          ))}
+        </Grid>
+      </div>
     </Container>
   );
 };
