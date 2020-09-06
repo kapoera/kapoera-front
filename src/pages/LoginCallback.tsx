cfec86e9e785c83c12ad9b5f3daadcfb8b8c64c9
pacokwon <haechank@gmail.com>
2020-09-06 23:51:48 +0900

    update: apply translations

--- src/pages/LoginCallback.tsx
+++ src/pages/LoginCallback.tsx
@@ -1,28 +1,50 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import { useIntl } from 'react-intl';
 import { useHistory, useLocation, Redirect } from 'react-router-dom';
-import { Button, Header, Modal } from 'semantic-ui-react';
+import { Button, Header, Icon, Modal } from 'semantic-ui-react';
+import styled from 'styled-components';
+import { GlobalContext, Actions } from '@/context';
 
 interface NicknameUpdateModalProps {
   defaultNickname: string;
 }
 
+const IconButton = styled(Button)`
+  background: transparent !important;
+  padding: 5px 10px !important;
+`;
+
 const NicknameUpdateModal: React.FC<NicknameUpdateModalProps> = ({
   defaultNickname
 }: NicknameUpdateModalProps) => {
   const history = useHistory();
+  const { formatMessage: f } = useIntl();
+  const { dispatch } = useContext(GlobalContext);
 
   return (
     <Modal open={true}>
-      <Header as="h1">Welcome, {defaultNickname}</Header>
+      <Header
+        as="div"
+        style={{ display: 'flex', justifyContent: 'space-between' }}
+      >
+        <h1 style={{ margin: 0 }}>
+          {f({ id: 'nicknamemodal.header' }, { nickname: defaultNickname })}
+        </h1>
+        <IconButton
+          icon
+          onClick={() => dispatch({ type: Actions.ToggleLocale })}
+        >
+          <Icon color="black" name="language" />
+        </IconButton>
+      </Header>
       <Modal.Content>
         <div style={{ fontSize: '1.5rem' }}>
-          To change the default nickname given to you, visit the profile page.
-          Note that nicknames are publicly exposed in this service.
+          {f({ id: 'nicknamemodal.content' })}
         </div>
       </Modal.Content>
       <Modal.Actions>
         <Button
-          content="To Main Page"
+          content={f({ id: 'nicknamemodal.button' })}
           labelPosition="right"
           icon="checkmark"
           onClick={() => {
